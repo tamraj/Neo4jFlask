a3fd2ae44da89cd88778979ddf4bd47774ed1690
Observert <yash0702@gmail.com>
2019-03-15 13:23:20 +0530

    Small changes with gitignore

--- app.py
+++ app.py
@@ -25,47 +25,5 @@ def getBusinessCenters():
     #data = [dict(record)['address'] for record in records]
     #return jsonify({'data':data})
 
-@app.route('/getAllRoles',methods=['POST','GET'])
-def getRoles():
-    session = driver.session()
-    query = "MATCH (n:Role) RETURN n"
-    records = session.run(query)
-    data = [dict(dict(i)['n'].items()) for i in records]
-    return jsonify(data)
-
-@app.route('/getAllPeople',methods=['POST','GET'])
-def getPeople():
-    session = driver.session()
-    query = "MATCH (n:Person) RETURN n"
-    records = session.run(query)
-    data = [dict(dict(i)['n'].items()) for i in records]
-    return jsonify(data)
-
-@app.route('/getPersonWithRole/<id>',methods=['POST','GET'])
-def getPeopleWithRole(id):
-    session = driver.session()
-    query = "MATCH (:Role{name:'"+id+"'})<-[IS]-(Person) RETURN Person"
-    records = session.run(query)
-    data = [dict(dict(i)['Person'].items()) for i in records]
-    return jsonify(data)
-
-@app.route('/getPersonWithBCenter/<id>',methods=['POST','GET'])
-def getPeopleWithBCenter(id):
-    session = driver.session()
-    query = "MATCH (:Business_Center{bc_number:'"+id+"'})<-[Works_For]-(Person) RETURN Person"
-    records = session.run(query)
-    data = [dict(dict(i)['Person'].items()) for i in records]
-    return jsonify(data)
-
-@app.route('/getPersonWithRoleUnderBCenter/<id>/<role>',methods=['POST','GET'])
-def getPersonWithRoleUnderBCenter(id=None,role=None):
-    session = driver.session()
-    query = "MATCH (:Business_Center{bc_number:'"+id+"'})<-[WORKS_FOR]-(Person)-[:IS]->" \
-                                                     "(:Role{name:'"+role+"'}) RETURN Person"
-    records = session.run(query)
-    data = [dict(dict(i)['Person'].items()) for i in records]
-    return jsonify(data)
-
-
 if __name__=='__main__':
     app.run(debug=True)
